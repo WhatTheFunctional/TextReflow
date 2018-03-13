2647b75a303bd7536ec477add1bd2d1529ec42fb
Laurence Emms <whatthefunctionalblog@gmail.com>
2018-03-13 14:10:24 -0700

    Removing unnecessary field in TextReflow.

--- TextReflow.hs
+++ TextReflow.hs
@@ -18,29 +18,29 @@ joinStrings joinString [] = ""
 joinStrings joinString (string : strings) = string ++ joinString ++ (joinStrings joinString strings)
 
 --Split string by a set of delimiters
-splitString :: [Char] -> [Char] -> String -> [String] -> [String]
-splitString _ [] [] allWords = [] --No characters remaining and no current word
-splitString _ [] currentWord allWords = (reverse currentWord) : allWords --No characters remaining and a current word exists
-splitString delimiters (c : cs) [] allWords
-    | c `elem` delimiters = splitString delimiters cs [] allWords --Splitting character detected and no current word exists
-    | otherwise = splitString delimiters cs [c] allWords --A non-splitting character was detected and no current word exists
-splitString delimiters (c : cs) currentWord allWords
-    | c `elem` delimiters = (reverse currentWord) : splitString delimiters cs [] allWords --Splitting character detected and a current word exists
-    | otherwise = splitString delimiters cs (c : currentWord) allWords --A non-splitting character was detected
+splitString :: [Char] -> [Char] -> String -> [String]
+splitString _ [] [] = [] --No characters remaining and no current word
+splitString _ [] currentWord = [reverse currentWord] --No characters remaining and a current word exists
+splitString delimiters (c : cs) []
+    | c `elem` delimiters = splitString delimiters cs [] --Splitting character detected and no current word exists
+    | otherwise = splitString delimiters cs [c] --A non-splitting character was detected and no current word exists
+splitString delimiters (c : cs) currentWord
+    | c `elem` delimiters = (reverse currentWord) : splitString delimiters cs [] --Splitting character detected and a current word exists
+    | otherwise = splitString delimiters cs (c : currentWord) --A non-splitting character was detected
 
 --Split string by a set of delimiters and keep the delimiters in the list
 --Only split when the previous character (head of current word) is a delimiter and the next character (head of string) is not a delimiter or
 --when the previous character is not a delimiter and the next character is a delimiter. This is equivalent to (delim a) xor (delim b) where (delim x)
 --is true if x is a delimiter
-splitStringWithDelimiters :: [Char] -> [Char] -> String -> [String] -> [String]
-splitStringWithDelimiters _ [] [] allWords = [] --No characters remaining and no current word
-splitStringWithDelimiters _ [] currentWord allWords = (reverse currentWord) : allWords --No characters remaining and a current word exists
-splitStringWithDelimiters delimiters (c : cs) [] allWords --Current word is empty
-    = splitStringWithDelimiters delimiters cs [c] allWords --Create new word with the current character
-splitStringWithDelimiters delimiters (c : cs) currentWord@(cw : cws) allWords
+splitStringWithDelimiters :: [Char] -> [Char] -> String -> [String]
+splitStringWithDelimiters _ [] [] = [] --No characters remaining and no current word
+splitStringWithDelimiters _ [] currentWord = [reverse currentWord] --No characters remaining and a current word exists
+splitStringWithDelimiters delimiters (c : cs) [] --Current word is empty
+    = splitStringWithDelimiters delimiters cs [c] --Create new word with the current character
+splitStringWithDelimiters delimiters (c : cs) currentWord@(cw : cws)
     | (not (c `elem` delimiters)) && (cw `elem` delimiters) ||
-      (c `elem` delimiters) && (not (cw `elem` delimiters)) = (reverse currentWord) : splitStringWithDelimiters delimiters (c : cs) [] allWords --New split detected and a current word exists
-    | otherwise = splitStringWithDelimiters delimiters cs (c : currentWord) allWords --A non-splitting character was detected, add it to the current word
+      (c `elem` delimiters) && (not (cw `elem` delimiters)) = (reverse currentWord) : splitStringWithDelimiters delimiters (c : cs) [] --New split detected and a current word exists
+    | otherwise = splitStringWithDelimiters delimiters cs (c : currentWord) --A non-splitting character was detected, add it to the current word
 
 --Split a single line after the column width limit
 splitLine :: Int -> Int -> [String] -> [String] -> ([String], [String])
@@ -60,7 +60,7 @@ reflowLine columnWidth words
 reflowLines :: [Char] -> Int -> [String] -> [String]
 reflowLines delimiters columnWidth [] = [] --No lines to reflow
 reflowLines delimiters columnWidth (line : lines)
-    = (reflowLine columnWidth (splitStringWithDelimiters delimiters line [] [])) ++ (reflowLines delimiters columnWidth lines)
+    = (reflowLine columnWidth (splitStringWithDelimiters delimiters line [])) ++ (reflowLines delimiters columnWidth lines)
 
 --Insert newlines into the reflowed lines, ignoring all lines which are followed by a delimiter line
 intercalateNewlines :: [String] -> String
@@ -73,7 +73,7 @@ intercalateNewlines (line1 : line2 : linesRemaining)
 reflowPutStr :: [Char] -> Int -> String -> IO ()
 reflowPutStr delimiters columnWidth line
     = putStr (intercalateNewlines (reflowLines delimiters columnWidth lines))
-        where lines = splitStringWithDelimiters ['\n'] line [] [] --Split into lines keeping existing newlines
+        where lines = splitStringWithDelimiters ['\n'] line [] --Split into lines keeping existing newlines
 
 reflowPutStrs :: [Char] -> Int -> [String] -> IO ()
 reflowPutStrs delimiters columnWidth lines = reflowPutStr delimiters columnWidth (concat lines)
@@ -81,7 +81,7 @@ reflowPutStrs delimiters columnWidth lines = reflowPutStr delimiters columnWidth
 reflowString :: [Char] -> Int -> String -> [String]
 reflowString delimiters columnWidth string
     = reflowLines delimiters columnWidth lines
-        where lines = splitStringWithDelimiters ['\n'] string [] []
+        where lines = splitStringWithDelimiters ['\n'] string []
 
 reflowStrings :: [Char] -> Int -> [String] -> [String]
 reflowStrings delimiters columnWidth strings = reflowString delimiters columnWidth (concat strings)
